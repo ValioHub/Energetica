aeebf7b1af3bc6fce98cdd4c5af7460d1ca89a9b
Valentin Chivliev <valentin.chivliev02@gmail.com>
2024-08-05 19:18:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Energetica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
